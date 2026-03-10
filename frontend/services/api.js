9d7d80f497e9cf7e26aafface35d80da1a6ed04d
Prashant Kumar Agrahari <109471438+agrahari1@users.noreply.github.com>
2026-03-10 07:49:50 +0530

    Fix string quotes in API baseURL definition

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || https://animall-tech-1.onrender.com,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://animall-tech-1.onrender.com',
 });
 
 export const startMilking = (data) => API.post("/start", data);
